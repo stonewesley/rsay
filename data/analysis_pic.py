33c471b6cfe2eeef8485d9164ea6e9caec26fe7a
hesiyi <hesiyi@baidu.com>
2017-07-04 21:02:36 +0800

    distribution of height and width

--- data/analysis_pic.py
+++ data/analysis_pic.py
@@ -1,16 +1,28 @@
 import os
 from PIL import Image
+import matplotlib.pyplot as plt
 
 pic_path = 'train_data/'
 filelist = os.listdir(pic_path)
-output = open('size_of_pic', 'w')
-
+# output = open('size_of_pic', 'w')
+height = []
+width = []
 for filename in filelist:
     filepath = pic_path + filename
     try:
         im = Image.open(filepath)
-        output.write(str(im.size[0]) + "," + str(im.size[1]) + "\n")
+        height.append(im.size[0])
+        width.append(im.size[1])
     except Exception, e:
         print Exception,":",e
-output.close()
+# output.close()
 
+fig = plt.figure()
+ax1 = fig.add_subplot(2,1,1)
+ax2 = fig.add_subplot(2,1,2)
+ax1.hist(height)
+ax1.set_title('distribution of height')
+ax2.hist(width)
+ax2.set_title("distribution of width")
+
+plt.savefig('distribution_of_h_and_w.jpg')
