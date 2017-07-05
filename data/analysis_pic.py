4f45a2d894a6b21bf92b038b37a17267eaf63e2c
hesiyi <hesiyi@baidu.com>
2017-07-05 15:09:48 +0800

    add ratio distribution

--- data/analysis_pic.py
+++ data/analysis_pic.py
@@ -1,6 +1,7 @@
 import os
 from PIL import Image
 import matplotlib.pyplot as plt
+import numpy as np
 
 pic_path = 'train_data/'
 filelist = os.listdir(pic_path)
@@ -18,11 +19,19 @@ for filename in filelist:
 # output.close()
 
 fig = plt.figure()
-ax1 = fig.add_subplot(2,1,1)
-ax2 = fig.add_subplot(2,1,2)
-ax1.hist(height)
+ax1 = fig.add_subplot(2,2,1)
+ax2 = fig.add_subplot(2,2,2)
+ax3 = fig.add_subplot(2,1,2)
+ax1.hist(height, range = (0, 1600))
 ax1.set_title('distribution of height')
-ax2.hist(width)
+ax2.hist(width, range = (0, 1200))
 ax2.set_title("distribution of width")
+a = np.array(height)
+b = np.array(width)
+a.dtype = "float64"
+b.dtype = "float64"
+c = a / b
+ax3.hist(c, range = (0.5, 2))
+ax3.set_title("distribution of ratio(height/width)")
 
 plt.savefig('distribution_of_h_and_w.jpg')
